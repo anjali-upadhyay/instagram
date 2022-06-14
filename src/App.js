6b35f8958aebded01d7693c2cdf54bd8a2141bd4
anjali-upadhyay <anjali_is19.rvitm@rvei.edu.in>
2022-06-14 19:17:15 +0530

    wt
    Co-Authored-By: kshiteesh28 <100955403+kshiteesh28@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import post from './post';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div className="app_header">
+      <img 
+      className="app_headerImage" 
+      src="https://logos-download.com/1210-instagram-logo-download.html" 
+      alt=""
+      />
+      </div>
+      <h1> hello</h1> 
     </div>
   );
 }
---
+++ src/post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './post.css';
+function post() {
+  return (
+    <div className="post">
+        <h3>Username</h3>
+        <img className="post_image" src='https://www.i-programmer.info/images/stories/News/2016/April/B/freecodecamp1.jpg'
+        alt=''/>
+        <h4>Username: Caption</h4>
+    </div>
+  )
+}
+
+export default post
